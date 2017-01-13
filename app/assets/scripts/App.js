a26bfab2aa201fcb389d33bcd7aaaabe8fa303f4
apple air <limahawkins@gmail.com>
2017-01-13 12:23:26 +0000

    finished modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -28,6 +28,9 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
+
+//instantiate objects
 var mobileMenu = new MobileMenu();
 //var revealOnScroll = new RevealOnScroll();
 
@@ -38,3 +41,5 @@ new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 
 var stickyHeader = new StickyHeader();
+
+var modal = new Modal();
